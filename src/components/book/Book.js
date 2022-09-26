20f186811711d774c97ac8564a7bc61e3751f173
Tanner Richards <Tanner.richards990@gmail.com>
2022-09-26 15:56:17 -0700

    added Link to return to catalog of books

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -1,5 +1,6 @@
 import Author from '../author/Author';
 import './Book.css';
+import { Link } from 'react-router-dom';
 
 function Book({
   book: { title, total_pages: totalPages, rating, isbn, published_date: publishedDate, authors },
@@ -7,6 +8,13 @@ function Book({
 }) {
   return (
     <article className="book">
+      {showDetail && (
+        <>
+          <Link to={`/Books`}>Return to the book catalog</Link>
+          <hr/>
+        
+        </>
+      )}
       <h3>{title}</h3>
       <address>
         {authors.map((author) => (
@@ -19,6 +27,7 @@ function Book({
           <p>ISBN: {isbn}</p>
           <p>Published {publishedDate}</p>
           <p>{totalPages} pages</p>
+          
         </>
       )}
     </article>
@@ -26,3 +35,4 @@ function Book({
 }
 
 export default Book;
+
