03673a0c128528f9f73f1724a574141d281998a1
Tanner Richards <Tanner.richards990@gmail.com>
2022-09-26 16:00:38 -0700

    built out routing in app js so the links will route to different pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import BookList from './components/book/BookList';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import BookDetail from './components/book/BookDetail';
+import Home from './Home';
 
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
-    </main>
+    <Router>
+      <Switch>
+        <Route exact path="/books">
+          <BookList />
+        </Route>
+        <Route exact path="/books/:id">
+          <BookDetail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+        
+      </Switch>
+    </Router>
+    
   );
 }
 
 export default App;
+
